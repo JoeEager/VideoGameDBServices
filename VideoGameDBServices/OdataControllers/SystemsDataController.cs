2982d435b4a92c4a2b8b81f918db3e1979c6d747
Joe <Joe.Eager.Github@outlook.com>
2018-07-09 22:06:42 -0400

    first odata controller

---
+++ VideoGameDBServices/OdataControllers/SystemsDataController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.OData;
+using System.Net;
+using VideoGameDBServices.Interfaces;
+
+
+namespace VideoGameDBServices.OdataControllers
+{
+    [Produces("application/json")]
+    [Route("odata/SystemsData")]
+    public class SystemsDataController : ODataController
+    {
+        private readonly ISystemRepository _systemRepository;
+
+        public SystemsDataController(ISystemRepository systemRepository)
+        {
+            _systemRepository = systemRepository;
+        }
+
+        // GET: odata/SystemsData
+        [EnableQuery]
+        public IActionResult GetSystems()
+        {
+            try
+            {
+                ObjectResult results = new ObjectResult(_systemRepository.GetAll())
+                {
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+
+                return results;
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+
+        }
+    }
+}
