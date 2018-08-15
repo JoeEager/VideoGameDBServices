4f60dbe40c8c76df4ac73a5158769d73ef2d366a
Joe <Joe.Eager.Github@outlook.com>
2018-08-14 22:05:47 -0400

    Updated test project folder structure. Set up first integration test.

---
+++ VideoGameDBServiceTests/IntegrationTests/DeveloperIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoGameDBServices.Models;
+using System.Collections.Generic;
+
+namespace VideoGameDBServiceTests
+{
+    [TestClass]
+    public class DeveloperIntegrationTests
+    {
+        private readonly HttpClient _client;
+
+        public DeveloperIntegrationTests()
+        {   
+            var testEnv = new VideoGameDBServiceTests.IntegrationTests.IntegrationTestBuilder();
+            _client = testEnv.ConfigureTests();
+        }
+
+        [TestMethod]
+        public void IntegrationTestDevelopers_GetAll()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod("GET"), "/api/Developers/");
+
+            // Act
+            var response = _client.SendAsync(request).Result;
+            var content = (response.Content.ReadAsAsync<List<Developers>>()).Result;
+  
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsTrue(content.Count > 0);
+        }
+    }
+}
---
+++ VideoGameDBServiceTests/IntegrationTests/IntegrationTestBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore;
+using VideoGameDBServices;
+using System.Text;
+
+
+namespace VideoGameDBServiceTests.IntegrationTests
+{
+    public class IntegrationTestBuilder
+    {
+        public HttpClient ConfigureTests()
+        {
+            var builder = WebHost.CreateDefaultBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>();
+            TestServer server = new TestServer(builder);
+            return(server.CreateClient());
+        }
+    }
+}
